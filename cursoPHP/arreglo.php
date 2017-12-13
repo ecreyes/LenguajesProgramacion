7520fffc655a5ca2609d622342f8df8722249db0
Eduardo Reyes <eduardo.reyeso.14@sansano.usm.cl>
2017-12-13 13:54:22 -0300

    Sintaxis del lenguaje de php
    Usando Php puro.

---
+++ cursoPHP/arreglo.php
@@ -0,0 +1,16 @@
+<?php
+#creando arreglo de [0-6]
+$semana = array('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');
+#añadiendo el elemento 7
+$semana[7]="Dia extra";
+echo $semana[7];
+
+#otra forma de escribir arreglos
+$colores = ['blanco','negro','azul'];
+echo $colores[0];
+
+#los arreglos pueden contener valores de cualquier tipo
+$arreglo = [1,'dos',true];
+echo $arreglo[2];
+
+?>
---
+++ cursoPHP/variables.php
@@ -0,0 +1,26 @@
+<?php
+#Char
+$letra = 'c';
+
+#String
+$nombre = "Eduardo";
+
+#Integer
+$numero = 7;
+
+#float
+$decimal = 7.7;
+
+#bool
+$var1 = true;
+$var2 = false;
+
+#imprimiendo variable con texto
+echo "hola, $nombre</br>";
+
+#concatenando variable
+echo "hola mi numero es ".$numero." y es de tipo entero </br>";
+
+#mostrar el tipo de variable
+echo gettype($letra);
+?>
